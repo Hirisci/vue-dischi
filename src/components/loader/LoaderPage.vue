1c5672852aaac109be2b76794372cbe90c0b3547
Hirisci <www.alan92@icloud.com>
2022-06-07 18:13:14 +0200

    aggiunta animazione

--- src/components/loader/LoaderPage.vue
+++ src/components/loader/LoaderPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- v-show="!isLoaded" -->
-  <div class="spinner">
+  <div class="spinner" v-show="!isLoaded">
     <div class="box"></div>
     <div class="box"></div>
     <div class="box"></div>
@@ -44,25 +44,36 @@ export default {
 
   @for $i from 1 to 5 {
     .box:nth-child(#{$i}) {
-      animation: animatename 1s linear infinite;
+      animation: animatename 4s linear infinite;
       animation-delay: #{$i}s;
     }
   }
 }
 
 .box {
-  height: 50px;
+  height: 30px;
+  border-radius: 100%;
+
+  border: 1px solid rgba(46, 189, 89, 0.2);
+
   aspect-ratio: 1/1;
-  background-color: black;
+  background-color: var(--clr-bg-1);
 }
 
 @keyframes animatename {
   0% {
     transform: scale(1);
+    background-color: var(--clr-bg-2);
   }
 
-  50% {
+  12% {
     transform: scale(1.5);
+  }
+
+  24% {
+    transform: scale(1);
+    margin: 0px;
+    background-color: var(--clr-bg-1);
   }
 }
 </style>
