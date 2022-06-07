ddff21ed41d64609e69e1a57962deec06cb62d5f
Hirisci <www.alan92@icloud.com>
2022-06-07 17:41:43 +0200

    bonus

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
+    <LoaderPage />
     <OHeader />
     <OMain />
   </div>
 </template>
 
 <script>
+import LoaderPage from "./components/loader/LoaderPage.vue";
 import OHeader from "./components/organis/OHeader.vue";
 import OMain from "./components/organis/OMain.vue";
 export default {
   name: "App",
-  components: { OHeader, OMain },
+  components: { OHeader, OMain, LoaderPage },
 };
 </script>
 
 <style lang="scss">
 @import "./assets/style/style.scss";
+#app {
+  position: relative;
+}
 </style>
---
+++ src/components/loader/LoaderPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- v-show="!isLoaded" -->
+  <div class="spinner">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoaderPage",
+  data: () => {
+    return {
+      isLoaded: false,
+    };
+  },
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        this.isLoaded = true;
+      }
+    };
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.spinner {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: var(--clr-bg-0);
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  z-index: 1;
+
+  @for $i from 1 to 5 {
+    .box:nth-child(#{$i}) {
+      animation: animatename 1s linear infinite;
+      animation-delay: #{$i}s;
+    }
+  }
+}
+
+.box {
+  height: 50px;
+  aspect-ratio: 1/1;
+  background-color: black;
+}
+
+@keyframes animatename {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.5);
+  }
+}
+</style>
+
